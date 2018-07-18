f10979040791d53b0df4404518300e4a8e66ee24
thevenine <seyeoyeniran2u@gmail.com>
2018-07-18 01:25:48 +0100

    Login form with client-side validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import HomePage from './components/pages/HomePage';
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 
 const App = () => (
-<div className="ui container">
+  <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
-</div> 
+  </div>
 );
-
 
 export default App;
